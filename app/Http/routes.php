259a9af3b942247ba418438f3f108b5d34958217
Chandresh <chandresh989@gmail.com>
2015-05-06 02:38:23 -0700

    fetched data from db and passed it to views to show the contents of articles

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,6 @@
 Route::get('about', 'PagesController@about');
 
 Route::get('contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
